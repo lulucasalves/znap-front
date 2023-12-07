8bb37f6513d519c9e2a85be3278d9c4093321e98
lucas <lucas.alves.supus@outlook.com>
2023-12-07 00:32:48 -0300

    refactor: envs

--- src/services/app.ts
+++ src/services/app.ts
@@ -1,5 +1,9 @@
 import axios, { AxiosInstance } from "axios";
 
+console.log(process.env);
 export const app: AxiosInstance = axios.create({
-  baseURL: "https://ill-lime-buffalo-tutu.cyclic.app",
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:8080"
+      : "https://ill-lime-buffalo-tutu.cyclic.app",
 });
