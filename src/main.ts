97166d421021ce6c24601cfb18c89d8f3ed125c9
lucas <lucas.alves.supus@outlook.com>
2023-12-10 15:44:45 -0300

    refactor: routes

--- src/main.ts
+++ src/main.ts
@@ -14,10 +14,12 @@ import App from "./App.vue";
 import { createApp } from "vue";
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
+import router from "./router";
 
 const app = createApp(App);
 
 app.use(Toast);
+app.use(router)
 registerPlugins(app);
 
 app.mount("#app");
